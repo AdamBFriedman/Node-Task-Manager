f039140a0adba5afd33423f8c5253318e8b8a9dc
Adam Friedman <adam.friedman5@gmail.com>
2021-12-16 11:59:57 -0600

    Add tasks and users collections to mongodb.

--- mongodb.js
+++ mongodb.js
@@ -18,10 +18,70 @@ MongoClient.connect(
     // Connection for specific database ("task-manager")
     const db = client.db(databaseName);
 
-    // Insert document into db
-    db.collection("users").insertOne({
-      name: "Adam",
-      age: 36,
-    });
+    /**
+     * Insert one document into db = insertOne()
+     * Insert multiple documents into db = insertMany()
+     */
+
+    // Add one user to database
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Adam",
+    //     age: 36,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert document.");
+    //     }
+
+    //     console.log(result.insertedId);
+    //   }
+    // );
+
+    // Add multiple users to database
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Courtney",
+    //       age: 30,
+    //     },
+    //     {
+    //       name: "Simba",
+    //       age: 9,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert documents.");
+    //     }
+
+    //     console.log(result.insertedIds);
+    //   }
+    // );
+
+    // Add tasks to database
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "Take trash out",
+          isCompleted: false,
+        },
+        {
+          description: "Feed Simba",
+          isCompleted: true,
+        },
+        {
+          description: "Clean litter",
+          isCompleted: true,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert documents.");
+        }
+
+        console.log(result.insertedIds);
+      }
+    );
   }
 );
