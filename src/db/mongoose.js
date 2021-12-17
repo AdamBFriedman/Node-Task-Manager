b4ac07717cee4ba7a7acae4dd97e9ee07fd093a8
Adam Friedman <adam.friedman5@gmail.com>
2021-12-17 09:58:31 -0600

    Add POST routes for /users and /tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,16 +3,3 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
 });
-
-// Create Task model
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  isCompleted: {
-    type: Boolean,
-    default: false,
-  },
-});
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 // This ensures that mongoose runs
 require("./db/mongoose");
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,7 +15,20 @@ app.post("/users", (request, response) => {
   user
     .save()
     .then(() => {
-      response.send(user);
+      response.status(201).send(user);
+    })
+    .catch((error) => {
+      response.status(400).send(error);
+    });
+});
+
+app.post("/tasks", (request, response) => {
+  const task = new Task(request.body);
+
+  task
+    .save()
+    .then(() => {
+      response.status(201).send(task);
     })
     .catch((error) => {
       response.status(400).send(error);
